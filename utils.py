c41af444d1d3de2c6de965654af567e1a10582e5
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-02-03 16:35:53 +0330

    writing a simple Controller class to avoid duplication

---
+++ utils.py
@@ -0,0 +1,34 @@
+import cv2 
+import time 
+
+class Controller : 
+    def __init__(self) : 
+        self.cap = cv2.VideoCapture(0) 
+        self.fps = Fps() 
+
+    def get_image(self) : 
+        sec , self.img = self.cap.read() 
+        return self.img if sec else None 
+
+    def show_image(self , militime = 1 , name = 'cam') : 
+        cv2.imshow(name , self.img) 
+        cv2.waitKey(militime)
+
+    def put_fps(self) :
+        cv2.putText(self.img , "FPS : " + str(int(self.fps.get_fps())) , (50, 50) , cv2.FONT_HERSHEY_COMPLEX , 1 , (0 , 0 , 255) , 2)		
+
+class Fps : 
+
+    def __init__(self) : 
+        self.prev_time = 0
+        self.curr_time = 0
+        self.fps = 0
+
+    def get_fps(self) : 
+        self.curr_time = time.time()
+        self.fps = 1 / (self.curr_time - self.prev_time) 
+        self.prev_time = self.curr_time 	
+        return self.fps
+
+
+ 
